be86061d2030341b8bb99ca66f91d1c97919d808
Aravinth Saminathan <aravinthnathan1@gmail.com>
2025-11-27 17:41:42 +0530

    complate all template Component problem

--- backend/routes/invoiceRoutes.js
+++ backend/routes/invoiceRoutes.js
@@ -2,16 +2,12 @@ const express = require("express");
 const router = express.Router();
 const invoiceController = require("../controllers/invoiceController");
 
-
 router.post("/", invoiceController.createInvoice);
 router.get("/", invoiceController.getInvoices);
 router.get("/:id", invoiceController.getInvoiceById);
 router.put("/:id", invoiceController.updateInvoice);
 router.delete("/:id", invoiceController.deleteInvoice);
 
-router.get('/:id/pdf', invoiceController.generateInvoicePDF);
-
-
-// router.get("/test-pdf/get", invoiceController.generateSamplePDF);
+router.get("/:id/pdf", invoiceController.generateInvoicePDF);
 
 module.exports = router;
